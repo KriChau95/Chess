b06e22b22fcbc828ad860b7f61ae82c6c4487e60
Krishaan Chaudhary <140979138+KriChau95@users.noreply.github.com>
2024-02-25 11:53:16 -0500

    Update Bishop.java

--- Bishop.java
+++ Bishop.java
@@ -1,10 +1,14 @@
 package chess;
 import java.lang.Math.*;
+
 public class Bishop extends Piece{
 
     private String symbol = "B";
+
+    // Constructor for Bishop class
     public Bishop(boolean white){
         super(white);
+        // set the symbol based on the color
         if (white){
             this.symbol = "w" + this.symbol;
         } else{
@@ -12,25 +16,28 @@ public class Bishop extends Piece{
         }
     }
 
+    // Override toString method to return the symbol of the bishop
     public String toString(){
         return this.symbol;
     }
-
+    
+    // Override isValid method to validate bishop moves
     @Override
     public boolean isValid(int startR, int startC, int endR, int endC, Board board) {
 
         int deltaR = endR - startR;
         int deltaC = endC - startC;
-        if (Math.abs(deltaR) != Math.abs(deltaC)){ // check if it lies on the diagonal
-            //System.out.println("illegal bishop move");
+        
+        // check if move is diagonal
+        if (Math.abs(deltaR) != Math.abs(deltaC)){ 
             return false;
         }
+        
         // It does lie on the diagonal. Must make sure no pieces in the way
         if (deltaR > 0 && deltaC > 0){
             int j = startR + 1;
             for (int i = startC + 1; i <= endC-1; i++){
                 if (board.board[j][i] != null){
-                    //System.out.println("illegal bishop move");
                     return false;
                 }
                 j++;
@@ -40,7 +47,6 @@ public class Bishop extends Piece{
             int j = startR + 1;
             for (int i = startC - 1; i >= endC+1; i--){
                 if (board.board[j][i] != null){
-                    //System.out.println("illegal bishop move");
                     return false;
                 }
                 j++;
@@ -50,7 +56,6 @@ public class Bishop extends Piece{
             int j = startR - 1;
             for (int i = startC - 1; i >= endC+1; i--){
                 if (board.board[j][i] != null){
-                    //System.out.println("illegal bishop move");
                     return false;
                 }
                 j--;
@@ -66,7 +71,7 @@ public class Bishop extends Piece{
                 j--;
             }
         }
-        //System.out.println("legal bishop move");
+        // If code makes it to here, it is a legal bishop move.
         return true;
     }
 }
