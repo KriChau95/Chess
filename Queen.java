ba30f481122ad16733185db3b6f05c31a6499b13
Krishaan Chaudhary <140979138+KriChau95@users.noreply.github.com>
2024-02-25 12:31:21 -0500

    Update Queen.java

--- Queen.java
+++ Queen.java
@@ -1,8 +1,12 @@
 package chess;
+
 import java.lang.Math.*;
+
 public class Queen extends Piece{
 
     private String symbol = "Q";
+
+    // Constructor for Queen class
     public Queen(boolean white){
         super(white);
         if (white){
@@ -12,11 +16,13 @@ public class Queen extends Piece{
         }
     }
 
+    // Overrides toString method and returns corresponding Queen string representation
     public String toString(){
         return this.symbol;
     }
 
     @Override
+    // Overrides Piece class' isValid method to determine legal queen move
     public boolean isValid(int startR, int startC, int endR, int endC, Board board) {
 
         int deltaR = endR - startR;
@@ -25,67 +31,69 @@ public class Queen extends Piece{
         if (Math.abs(deltaR) != Math.abs(deltaC)){ // check if it does not lie on the diagonal
             // check if it might be an orthogonal move
             if (startR != endR && startC != endC) {
-                //System.out.println("Illegal move: Must move along a rank or file.");
+                // illegal motion in terms of direction
                 return false;
             }
 
             int rowIncrement = (startR != endR) ? ((endR > startR) ? 1 : -1) : 0;
             int colIncrement = (startC != endC) ? ((endC > startC) ? 1 : -1) : 0;
-
+            
+            // checking if piece in the way of move
             for (int i = startR + rowIncrement, j = startC + colIncrement; i != endR || j != endC; i += rowIncrement, j += colIncrement) {
                 if (board.board[i][j] != null) {
-                    //System.out.println("Illegal orthogonal move: Piece in the way.");
+                    // Illegal orthogonal move: Piece in the way
                     return false;
                 }
             }
-
-            //System.out.println("Legal orthogonal move");
+            // legal orthogonal move
             return true;
         }
+        
         // It does lie on the diagonal. Must make sure no pieces in the way
         if (deltaR > 0 && deltaC > 0){
             int j = startR + 1;
             for (int i = startC + 1; i <= endC-1; i++){
                 if (board.board[j][i] != null){
-                    //System.out.println("illegal diagonal move");
+                    // illegal diagonal move
                     return false;
                 }
                 j++;
             }
         }
+        
         if (deltaR > 0 && deltaC < 0){
             int j = startR + 1;
             for (int i = startC - 1; i >= endC+1; i--){
                 if (board.board[j][i] != null){
-                    //System.out.println("illegal diagonal move");
+                    // illegal diagaonal move
                     return false;
                 }
                 j++;
             }
         }
+        
         if (deltaR < 0 && deltaC < 0){
             int j = startR - 1;
             for (int i = startC - 1; i >= endC+1; i--){
                 if (board.board[j][i] != null){
-                    //System.out.println("illegal diagonal move");
+                    // illegal diagaonal move
                     return false;
                 }
                 j--;
             }
         }
+        
         if (deltaR < 0 && deltaC > 0){
             int j = startR - 1;
             for (int i = startC + 1; i <= endC-1; i++){
                 if (board.board[j][i] != null){
-                    //System.out.println("illegal diagonal move");
+                    illegal diagonal move
                     return false;
                 }
                 j--;
             }
         }
-        //System.out.println("legal diagonal move");
+        // legal diagonal move
         return true;
-
     }
-
 }
