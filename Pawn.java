29afa310c754a71eb195b4e100786829543f924b
Krishaan Chaudhary <140979138+KriChau95@users.noreply.github.com>
2024-02-25 12:19:29 -0500

    Update Pawn.java

--- Pawn.java
+++ Pawn.java
@@ -2,6 +2,8 @@ package chess;
 
 public class Pawn extends Piece{
     private String symbol = "p";
+
+    // Pawn class constructor
     public Pawn (boolean white){
         super(white);
         if (white){
@@ -10,13 +12,16 @@ public class Pawn extends Piece{
             this.symbol = "b" + this.symbol;
         }
     }
+
+    // Returns string to represent pawn
     public String toString(){
         return this.symbol;
     }
+
+    // Overriding the Piece class' isValid method to check for legal pawn moves
     public boolean isValid(int startR, int startC, int endR, int endC, Board board) {
 
         // enpassant
-
         if (board.board[startR][startC].white){ // white pawn trying en passant
             if (startR == 3 && endR == 2){
                 if (Math.abs(endC - startC) == 1){
@@ -45,42 +50,36 @@ public class Pawn extends Piece{
             if (endR == startR - 1){ //  move results in one row advanced
                 if (endC == startC){ // move one space straight forward
                     if(board.board[endR][endC] == null){ // make sure there is room - empty space
-                        //System.out.println("White pawn advance by one");
                         return true;
-                        // consider promotion case
-                    } else {
-                        //System.out.println("no room for one space white pawn advance");
+                    } else { // no room for one space white pawn advance
                         return false;
                     }
                 } else if(Math.abs(endC - startC) == 1){ // attempt at diagonal capture one row forward
                     if (board.board[endR][endC] != null && !(board.board[endR][endC].white)){ // if not empty and black piece
-                        //System.out.println("White pawn captured smth");
+                        // white pawn successfully captured something
                         return true;
-                        // consider promotion case
                     } else {
-                        //System.out.println("White pawn can't diagonally move into ally/empty square");
+                        // white pawn trying to diagonally move into ally/empty space
                         return false;
                     }
                 } else {
-                    //System.out.println("White pawn cannot teleport like that");
+                    // other illegal white pawn motion
                     return false;
                 }
             } else if (endR == startR - 2 && startC == endC){ // attempting 2 space pawn advance - only allowed on first move
                 if (startR == 6){ // on appropriate white starting row
                     if (board.board[startR-1][startC] == null && board.board[endR][endC] == null){ // clear path
-                        //System.out.println("White pawn 2 space advance");
                         return true;
-                        // consider promotion case
                     } else {
-                        //System.out.println("pieces in the way of white pawn 2 space advance");
+                        // pieces in the way of white pawn 2 space advance
                         return false;
                     }
                 } else {
-                    //System.out.println("White pawn can only move 2 forward on starting row");
+                    // Some other illegal pawn move
                     return false;
                 }
             } else {
-                //System.out.println("Some other illegal white pawn move");
+                // Some other illegal pawn move
                 return false;
             }
 
@@ -89,42 +88,38 @@ public class Pawn extends Piece{
             if (endR == startR + 1){ //  move results in one row advanced
                 if (endC == startC){ // move one space straight forward
                     if(board.board[endR][endC] == null){ // make sure there is room - empty space
-                        //System.out.println("Black pawn advance by one");
                         return true;
-                        // consider promotion case
                     } else {
-                        //System.out.println("no room for one space black pawn advance");
+                        // no room for one space black pawn advance
                         return false;
                     }
                 } else if(Math.abs(endC - startC) == 1){ // attempt at diagonal capture one row forward
                     if (board.board[endR][endC] != null && board.board[endR][endC].white){ // if not empty and white piece
-                        //System.out.println("Black pawn captured smth");
+                        // black pawn successful capture
                         return true;
-                        // consider promotion case
                     } else {
-                        //System.out.println("Black pawn can't diagonally move into ally/empty square");
+                        // black pawn can't diagonally move into ally/empty square
                         return false;
                     }
                 } else {
-                    //System.out.println("Black pawn cannot teleport like that");
+                    // illegal black pawn move
                     return false;
                 }
             } else if (endR == startR + 2 && startC == endC){ // attempting 2 space pawn advance - only allowed on first move
                 if (startR == 1){ // on appropriate black starting row
                     if (board.board[startR+1][startC] == null && board.board[endR][endC] == null){ // clear path
-                        //System.out.println("Black pawn 2 space advance");
+                        // successful black pawn 2 space advance
                         return true;
-                        // consider promotion case
                     } else {
-                        //System.out.println("pieces in the way of black pawn 2 space advance");
+                        // pieces in the way of black pawn 2 space advance
                         return false;
                     }
                 } else {
-                    //System.out.println("Black pawn can only move 2 forward on starting row");
+                    // some other illegal black pawn move;
                     return false;
                 }
             }  else {
-                //System.out.println("Some other illegal black pawn move");
+                // some other illegal black pawn move;
                 return false;
             }
         }
