e0bb88683c2261c2ffb51e65ee43ae1fbeeaa9bb
Krishaan Chaudhary <140979138+KriChau95@users.noreply.github.com>
2024-02-25 12:13:04 -0500

    Update King.java

--- King.java
+++ King.java
@@ -3,6 +3,8 @@ import java.lang.Math.*;
 public class King extends Piece{
 
     private String symbol = "K";
+
+    // Constructor to initialize King based on color
     public King(boolean white){
         super(white);
         if (white){
@@ -12,16 +14,16 @@ public class King extends Piece{
         }
     }
 
+    // Overrides toString method to return String representation of King
     public String toString(){
         return this.symbol;
     }
 
     @Override
+    // Overriding the isValid method from Piece to determine if a king move is valid
     public boolean isValid(int startR, int startC, int endR, int endC, Board board) {
 
-        //Check castling
-        //Check if threatened
-        //Check if within 1 space
+        // Ensure move is only 1 square from starting square
         if (Math.abs(endC-startC) <=1 && Math.abs(endR-startR) <=1){
             if (!(startR == endR && startC == endC)){
                 firstMove = false;
@@ -30,32 +32,29 @@ public class King extends Piece{
         }
 
         // castling
-
         if (startR == 7 && startC == 4 && endR == 7 && endC == 6 && board.wks){ // white king side castling attempt
             for (int r = 0; r < 8; r++){
                 for (int c = 0; c < 8; c++){
                     if (board.board[r][c] != null && !(board.board[r][c].white)){ // this piece is black
+                        // checking if a black piece is threatening one of the squares the king must castle through
                         if (board.board[r][c].isValid(r,c, 7 ,4, board)){
-                            //System.out.println("can't castle");
                             return false;
                         }
                         if (board.board[r][c].isValid(r,c, 7 ,5, board)){
-                            //System.out.println("can't castle");
                             return false;
                         }
                         if (board.board[r][c].isValid(r,c, 7 ,6, board)){
-                            //System.out.println("can't castle");
                             return false;
                         }
                     }
                 }
             }
+            // checking if pieces in the way of castling attempt
             if (!(board.board[7][5] == null && board.board[7][6] == null)){
-                //System.out.println("can't castle. pieces in the way");
                 return false;
             }
+            // checking if corresponding rook exists
             if (!(board.board[7][7] instanceof Rook && board.board[7][7].white)){
-                //System.out.println("Must have white rook to castle with");
                 return false;
             }
             return true;
@@ -65,27 +64,25 @@ public class King extends Piece{
             for (int r = 0; r < 8; r++){
                 for (int c = 0; c < 8; c++){
                     if (board.board[r][c] != null && !(board.board[r][c].white)){ // this piece is black
+                        // checking if a black piece is threatening one of the squares the king must castle through
                         if (board.board[r][c].isValid(r,c, 7 ,4, board)){
-                            //System.out.println("can't castle");
                             return false;
                         }
                         if (board.board[r][c].isValid(r,c, 7 ,3, board)){
-                            //System.out.println("can't castle");
                             return false;
                         }
                         if (board.board[r][c].isValid(r,c, 7 ,2, board)){
-                            //System.out.println("can't castle");
                             return false;
                         }
                     }
                 }
             }
+            // checking if pieces in the way of castle
             if (!(board.board[7][1] == null && board.board[7][2] == null && board.board[7][3] == null)){
-                //System.out.println("can't castle. pieces in the way");
                 return false;
             }
+            // checking if corresponding rook exists
             if (!(board.board[7][0] instanceof Rook && board.board[7][0].white)){
-                //System.out.println("Must have white rook to castle with");
                 return false;
             }
             return true;
@@ -94,28 +91,26 @@ public class King extends Piece{
         if (startR == 0 && startC == 4 && endR == 0 && endC == 6 && board.bks){ // black king side castling attempt
             for (int r = 0; r < 8; r++){
                 for (int c = 0; c < 8; c++){
+                    // checking if a white piece is threatening one of the squares the king must castle through
                     if (board.board[r][c] != null && board.board[r][c].white){ // this piece is white
                         if (board.board[r][c].isValid(r,c, 0 ,4, board)){
-                            //System.out.println("can't castle");
                             return false;
                         }
                         if (board.board[r][c].isValid(r,c, 0 ,5, board)){
-                            //System.out.println("can't castle");
                             return false;
                         }
                         if (board.board[r][c].isValid(r,c, 0 ,6, board)){
-                            //System.out.println("can't castle");
                             return false;
                         }
                     }
                 }
             }
+            // checking if piece in the way
             if (!(board.board[0][5] == null && board.board[0][6] == null)){
-                //System.out.println("can't castle. pieces in the way");
                 return false;
             }
+            // checking if corresponding rook exists
             if (!(board.board[0][7] instanceof Rook && !(board.board[0][7].white))){
-                //System.out.println("Must have black rook to castle with");
                 return false;
             }
             return true;
@@ -125,33 +120,31 @@ public class King extends Piece{
             for (int r = 0; r < 8; r++){
                 for (int c = 0; c < 8; c++){
                     if (board.board[r][c] != null && board.board[r][c].white){ // this piece is white
+                        // checking if a white piece is threatening one of the squares the king must castle through
                         if (board.board[r][c].isValid(r,c, 0 ,4, board)){
-                            //System.out.println("can't castle");
                             return false;
                         }
                         if (board.board[r][c].isValid(r,c, 0 ,3, board)){
-                            //System.out.println("can't castle");
                             return false;
                         }
                         if (board.board[r][c].isValid(r,c, 0,2, board)){
-                            //System.out.println("can't castle");
                             return false;
                         }
                     }
                 }
             }
+            // checking if pieces in the way of castling
             if (!(board.board[0][1] == null && board.board[0][2] == null && board.board[0][3] == null)){
-                //System.out.println("can't castle. pieces in the way");
                 return false;
             }
+            // checking if corresponding rook exists
             if (!(board.board[0][0] instanceof Rook && !(board.board[0][0].white))){
-                //System.out.println("Must have black rook to castle with");
                 return false;
             }
             return true;
         }
 
-        //System.out.println("Illegal King Move");
+        // If we have made it to here, then all the legal king rook moves have been checked for, and it must be an illegal king move
         return false;
     }
 }
