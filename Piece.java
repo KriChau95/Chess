53af1679123b61b595d471e84330823d2f19e998
Krishaan Chaudhary <140979138+KriChau95@users.noreply.github.com>
2024-02-25 12:24:09 -0500

    Update Piece.java

--- Piece.java
+++ Piece.java
@@ -1,10 +1,17 @@
 package chess;
 
+// Abstract class to store common data for all Pieces and treat Pieces similarly
 public abstract class Piece {
+    
     public boolean white = true;
     public boolean firstMove = true;
+
+    // abstract isValid method - to be implemented by child subclasses
     public abstract boolean isValid(int startR, int startC, int endR, int endC, Board board);
+
+    // constructor that initializes piece color
     public Piece(boolean color){
         white = color;
     }
+    
 }
